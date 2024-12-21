7ffa40f1bb606f4edd5545e7603be4a88fc13fd9
Joe McCarthy <179146301+joe-mccarthy@users.noreply.github.com>
2024-12-21 00:16:03 +0000

    adding release script

--- harlow_bindicator/main.py
+++ harlow_bindicator/main.py
@@ -18,8 +18,7 @@ def fetch_bin_collection():
         "--uprn", type=str, required=True, help="Property Reference Number"
     )
     parser.add_argument("--topic", type=str, required=True, help="Ntfy Topic")
-    args = parser.parse_args()
-    print(args)
+    args = parser.parse_args()    
     bindicator = Bindicator(args.uprn, args.topic)
     bindicator.run()
     logging.info("Harlow Bindicator run completed")
